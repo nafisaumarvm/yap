6d0216348ecaf1e6a7d25e6a9d25975b3e750005
Nafisa Umar <nafisahsokumar@gmail.com>
2026-05-13 21:44:39 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,20 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import SubmitPage from "./pages/SubmitPage";
+import VotePage from "./pages/VotePage";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  <BrowserRouter>
+   <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/submit" element={<SubmitPage />} />
+      <Route path="/vote" element={<VotePage />} />
+    </Routes>
+  </BrowserRouter>
 );
